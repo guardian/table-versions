27ea76f8652ea9729ec9af40e767d98c35b45792
romain_gallet <romain.gallet.freelancer@gmail.com>
2019-04-30 15:47:19 +0100

    wip

--- spark/src/test/scala/org/apache/hadoop/fs/versioned/VersionedFileSystemSpec.scala
+++ spark/src/test/scala/org/apache/hadoop/fs/versioned/VersionedFileSystemSpec.scala
@@ -1,16 +1,15 @@
 package org.apache.hadoop.fs.versioned
 
+import java.net.URI
+
 import com.gu.tableversions.core.Partition
 import com.gu.tableversions.core.Partition.{ColumnValue, PartitionColumn}
-import com.gu.tableversions.spark.SparkHiveSuite
-import org.apache.spark.sql.SaveMode
+import org.apache.hadoop.fs.Path
 import org.scalatest.{FlatSpec, Matchers}
 
-class VersionedFileSystemSpec extends FlatSpec with Matchers with SparkHiveSuite {
+class VersionedFileSystemSpec extends FlatSpec with Matchers {
 
-  spark.sparkContext.setLogLevel("ERROR")
-
-  "flattening List[Partition]" should "produce configuration entries" in {
+  "partitionMappings" should "produce configuration entries for each partition" in {
 
     val m =
       List(Partition(ColumnValue(PartitionColumn("date"), "2019-01-01"), ColumnValue(PartitionColumn("hour"), "01")))
@@ -20,46 +19,15 @@ class VersionedFileSystemSpec extends FlatSpec with Matchers with SparkHiveSuite
 
   }
 
-  "VersionedFileSystem" should "write partitions with a version suffix" in {
-    import spark.implicits._
+  "appendVersion" should "add a version to a given path" ignore {
 
-    spark.sparkContext.hadoopConfiguration.set("fs.versioned.baseFS", "file")
-    spark.sparkContext.hadoopConfiguration.set("fs.versioned.version", "version1")
+    val fs = new VersionedFileSystem
+    fs.initialize(new URI(s"file:///tmp/test"), ???)
 
-    val path = tableUri.resolve(s"table/").toString.replace("file:", "versioned://")
+    val path = new Path(new URI(s"file:///tmp/test"))
 
-    val partitionsValues =
-      List(
-        Partition(ColumnValue(PartitionColumn("date"), "2019-01-01"), ColumnValue(PartitionColumn("hour"), "01")),
-        Partition(ColumnValue(PartitionColumn("date"), "2019-01-01"), ColumnValue(PartitionColumn("hour"), "02")),
-        Partition(ColumnValue(PartitionColumn("date"), "2019-01-01"), ColumnValue(PartitionColumn("hour"), "03")),
-        Partition(ColumnValue(PartitionColumn("date"), "2019-01-01"), ColumnValue(PartitionColumn("hour"), "04")),
-        Partition(ColumnValue(PartitionColumn("date"), "2019-01-01"), ColumnValue(PartitionColumn("hour"), "05"))
-      )
+//    VersionedFileSystem.partitionMappings(m) shouldBe Map(
+//      "versioned__date=2019-01-01/hour=01" -> "date=2019-01-01/hour=01")
 
-    VersionedFileSystem.partitionMappings(partitionsValues) foreach {
-      case (k, v) => spark.sparkContext.hadoopConfiguration.set(k, v)
-    }
-
-    List(TestRow(1, "2019-01-01", "01"),
-         TestRow(2, "2019-01-01", "02"),
-         TestRow(3, "2019-01-01", "03"),
-         TestRow(4, "2019-01-01", "04"),
-         TestRow(5, "2019-01-01", "05")).toDS.write
-      .mode(SaveMode.Append)
-      .partitionBy("date", "hour")
-      .parquet(path)
-
-    val rows = spark.read.parquet(path).as[TestRow].collect.toList
-
-    rows should contain theSameElementsAs List(
-      TestRow(1, "2019-01-01", "1"),
-      TestRow(2, "2019-01-01", "2"),
-      TestRow(3, "2019-01-01", "3"),
-      TestRow(4, "2019-01-01", "4"),
-      TestRow(5, "2019-01-01", "5")
-    )
   }
 }
-
-case class TestRow(value: Int, date: String, hour: String)
