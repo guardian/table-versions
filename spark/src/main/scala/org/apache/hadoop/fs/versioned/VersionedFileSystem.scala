4f6144b626659c0dfcb503a1f6a0c1ccc7995540
romain_gallet <romain.gallet.freelancer@gmail.com>
2019-04-30 12:26:43 +0100

    wip

--- spark/src/main/scala/org/apache/hadoop/fs/versioned/VersionedFileSystem.scala
+++ spark/src/main/scala/org/apache/hadoop/fs/versioned/VersionedFileSystem.scala
@@ -4,7 +4,6 @@ import java.net.URI
 import java.util.Objects
 
 import com.gu.tableversions.core.Partition
-import com.gu.tableversions.metastore.VersionPaths
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs._
 import org.apache.hadoop.fs.permission.FsPermission
@@ -14,7 +13,7 @@ class VersionedFileSystem extends FileSystem {
 
   private var baseURI: URI = _
   private var baseFS: FileSystem = _
-  private var mappings: Map[String, String] = _
+  private var partitionMappings: Map[String, String] = _
   private var version: String = _
 
   override def initialize(name: URI, conf: Configuration): Unit = {
@@ -28,7 +27,7 @@ class VersionedFileSystem extends FileSystem {
 
     import scala.collection.JavaConverters._
 
-    mappings = conf.iterator.asScala
+    partitionMappings = conf.iterator.asScala
       .filter(_.getKey.startsWith(VersionedFileSystem.KEY_PREFIX))
       .map(_.getValue)
       .toList
@@ -45,9 +44,8 @@ class VersionedFileSystem extends FileSystem {
     baseFS.initialize(baseURI, conf)
   }
 
-  override def getUri: URI = {
+  override def getUri: URI =
     new URI(s"${VersionedFileSystem.SCHEME}:${baseURI.getSchemeSpecificPart}")
-  }
 
   override def open(f: Path, bufferSize: Int): FSDataInputStream =
     baseFS.open(baseVersionedPath(f), bufferSize)
@@ -71,16 +69,20 @@ class VersionedFileSystem extends FileSystem {
   override def delete(f: Path, recursive: Boolean): Boolean =
     baseFS.delete(baseVersionedPath(f), recursive)
 
+  private def toggleFileStatus(fileStatus: FileStatus, isParam: Boolean): FileStatus = {
+    new FileStatus(
+      fileStatus.getLen,
+      fileStatus.isDirectory,
+      fileStatus.getReplication.toInt,
+      fileStatus.getBlockSize,
+      fileStatus.getModificationTime,
+      if (isParam) baseVersionedPath(fileStatus.getPath) else versionedPath(fileStatus.getPath)
+    )
+  }
+
   override def listStatus(f: Path): Array[FileStatus] =
     baseFS.listStatus(baseVersionedPath(f)) map { fileStatus =>
-      new FileStatus(
-        fileStatus.getLen,
-        fileStatus.isDirectory,
-        fileStatus.getReplication.toInt,
-        fileStatus.getBlockSize,
-        fileStatus.getModificationTime,
-        versionedPath(fileStatus.getPath)
-      )
+      toggleFileStatus(fileStatus, false)
     } filter { fileStatus =>
       fileStatus.getPath.toUri.getSchemeSpecificPart.contains(version) ||
       fileStatus.isDirectory
@@ -94,29 +96,61 @@ class VersionedFileSystem extends FileSystem {
   override def mkdirs(f: Path, permission: FsPermission): Boolean =
     baseFS.mkdirs(baseVersionedPath(f), permission)
 
-  override def getFileStatus(f: Path): FileStatus = {
-    val fileStatus = baseFS.getFileStatus(baseVersionedPath(f))
+  override def getFileStatus(f: Path): FileStatus =
+    toggleFileStatus(baseFS.getFileStatus(baseVersionedPath(f)), false)
 
-    new FileStatus(
-      fileStatus.getLen,
-      fileStatus.isDirectory,
-      fileStatus.getReplication.toInt,
-      fileStatus.getBlockSize,
-      fileStatus.getModificationTime,
-      versionedPath(fileStatus.getPath)
-    )
+  override def getFileBlockLocations(file: FileStatus, start: Long, len: Long): Array[BlockLocation] = {
+    super.getFileBlockLocations(toggleFileStatus(file, true), start, len)
   }
+
+  override def makeQualified(f: Path): Path = baseFS.makeQualified(baseVersionedPath(f))
+
+  override def checkPath(f: Path): Unit = super.checkPath(baseVersionedPath(f))
+
+  override def setReplication(f: Path, replication: Short): Boolean =
+    baseFS.setReplication(baseVersionedPath(f), replication)
+
+  override def copyFromLocalFile(src: Path, dst: Path): Unit =
+    baseFS.copyFromLocalFile(baseVersionedPath(src), baseVersionedPath(dst))
+
+  override def copyFromLocalFile(delSrc: Boolean, src: Path, dst: Path): Unit =
+    baseFS.copyFromLocalFile(delSrc, baseVersionedPath(src), baseVersionedPath(dst))
+
+  override def copyFromLocalFile(delSrc: Boolean, overwrite: Boolean, srcs: Array[Path], dst: Path): Unit =
+    baseFS.copyFromLocalFile(delSrc, overwrite, srcs, baseVersionedPath(dst))
+
+  override def copyFromLocalFile(delSrc: Boolean, overwrite: Boolean, src: Path, dst: Path): Unit =
+    baseFS.copyFromLocalFile(delSrc, overwrite, src, baseVersionedPath(dst))
+
+  override def copyToLocalFile(src: Path, dst: Path): Unit = baseFS.copyToLocalFile(src, dst)
+
+  override def deleteOnExit(f: Path): Boolean = baseFS.deleteOnExit(baseVersionedPath(f))
+
+  override def getHomeDirectory: Path = versionedPath(baseFS.getHomeDirectory)
+
+  override def startLocalOutput(fsOutputFile: Path, tmpLocalFile: Path): Path =
+    baseFS.startLocalOutput(fsOutputFile, tmpLocalFile)
+
+  override def getFileChecksum(f: Path): FileChecksum = super.getFileChecksum(baseVersionedPath(f))
+
+  override def setOwner(f: Path, username: String, groupname: String): Unit =
+    super.setOwner(baseVersionedPath(f), username, groupname)
+
+  override def setTimes(f: Path, mtime: Long, atime: Long): Unit = super.setTimes(baseVersionedPath(f), mtime, atime)
+
+  override def setPermission(f: Path, permission: FsPermission): Unit =
+    super.setPermission(baseVersionedPath(f), permission)
 
   private lazy val appendVersion: Path => Path = path => {
     val uri = path.toUri
 
-    val sp = mappings.foldLeft(uri.getSchemeSpecificPart) {
+    val specificPart = partitionMappings.foldLeft(uri.getSchemeSpecificPart) {
       case (acc, (partition, versionedPartition)) if !acc.contains(versionedPartition) && acc.contains(partition) =>
         acc.replace(partition, versionedPartition)
       case (acc, _) => acc
     }
 
-    new Path(new URI(s"${uri.getScheme}:$sp"))
+    new Path(new URI(s"${uri.getScheme}:$specificPart"))
   }
 
   private lazy val setScheme: String => Path => Path =
